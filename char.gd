1810dbb467a1d6564aa2422e5c8a8ada71c38286
jacobspeicher <speicherjacob@gmail.com>
2018-08-07 20:13:22 -0400

    Made a first pass at the splitting mechanic. If you jump just right or run into the red blocks you'll split. If you run two of the player characters together, they'll merge.

--- char.gd
+++ char.gd
@@ -1,18 +1,47 @@
 extends KinematicBody2D
 
-export (int) var run_speed = 100
-export (int) var jump_speed = -400
+export (int) var run_speed = 225
+export (int) var jump_speed = -415
+export (int) var bounce_back_force = 300
 export (int) var gravity = 1200
+export (float) var size = 1.0
+export (bool) var able_to_merge = false
+export (bool) var hit = false
+export (bool) var dead = false
 
-var velocity = Vector2()
-var jumping = false
-var initial_char_pos = get_global_position()
+var velocity
+var jumping
+var initial_char_pos
+var hit_object
 
+func _ready():
+	velocity = Vector2()
+	jumping = false
+	initial_char_pos = get_global_position()
+	set_physics_process(true)
+		
+func _physics_process(delta):
+	get_input()
+	velocity.y += gravity * delta
+	if jumping and is_on_floor():
+		jumping = false
+	
+	if get_slide_count() > 0:
+		var collision = get_slide_collision(get_slide_count() - 1)
+		hit_object = collision.collider
+		if hit_object.get_collision_layer() == 1:
+			able_to_merge = true
+		if hit_object.get_collision_layer() == 32:
+			set_is_hit(true)
+			velocity += collision.get_normal() * bounce_back_force
+			
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+	
 func get_input():
 	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
-	var jump = Input.is_action_just_pressed('ui_select')
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('jump')
 
 	if jump and is_on_floor():
 		jumping = true
@@ -23,16 +52,31 @@ func get_input():
 			velocity.x += run_speed
 		if left:
 			velocity.x -= run_speed
-		
-func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
-	if jumping and is_on_floor():
-		jumping = false
+
+func get_player_scale():
+	return size
 	
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+func get_is_hit():
+	return hit
+
+func get_is_able_to_merge():
+	return able_to_merge
 	
-	if get_slide_count() > 0:
-		var hit = get_slide_collision(get_slide_count() - 1).collider
-		if hit.get_collision_layer() == 32:
-			set_global_position(initial_char_pos)
+func get_other_player():
+	if able_to_merge:
+		return hit_object
+			
+func set_player_scale(new_scale):
+	size = new_scale
+	set_scale(Vector2(new_scale, new_scale))
+	
+func set_is_hit(new_hit):
+	hit = new_hit
+	
+func set_is_able_to_merge(new_able_to_merge):
+	able_to_merge = new_able_to_merge
+	
+func set_is_dead():
+	remove_child(get_node("CollisionShape2D"))
+	set_physics_process(false)
+	queue_free()
---
+++ platformer_rules.gd
@@ -0,0 +1,75 @@
+extends Node
+
+var player_children = []
+var is_over = false
+
+func _ready():
+	var player = load('res://player.tscn')
+	var player_instance = player.instance()
+	
+	var start = get_parent().get_node('root/start')
+	var start_pos = start.get_global_position()
+	
+	player_instance.set_name('player0')
+	player_instance.set_global_position(Vector2(start_pos.x + 30, start_pos.y))
+	player_children.append(player_instance)
+	add_child(player_instance)
+	set_physics_process(true)
+	
+func _physics_process(delta):
+	if not player_children:
+		is_over = true
+		game_over()
+		
+	for child in player_children:
+		if child.get_is_hit():
+			if child.get_player_scale() > 0.25:
+				var cur_scale = child.get_player_scale()
+				var new_scale
+				if cur_scale in [1.0, 0.5]:
+					new_scale = child.get_player_scale() / 2.0
+				else:
+					new_scale = child.get_player_scale() - 0.25
+				
+				child.set_is_hit(false)
+				child.set_player_scale(new_scale)
+				
+				var new_player = load('res://player.tscn')
+				var new_player_instance = new_player.instance()
+				var new_player_name = 'player' + str(len(player_children))
+				print(child.get_global_position())
+				var timer = Timer.new()
+				timer.one_shot = true
+				timer.wait_time = 1
+				timer.start()
+				
+				var new_player_pos = Vector2(child.get_global_position().x, child.get_global_position().y)
+				print(new_player_pos)
+				new_player_instance.set_name(new_player_name)
+				new_player_instance.set_global_position(new_player_pos)
+				new_player_instance.set_player_scale(cur_scale - new_scale)
+				
+				player_children.append(new_player_instance)
+				add_child(new_player_instance)
+			else:
+				child.set_is_dead()
+				player_children.erase(child)
+				
+		if child.get_is_able_to_merge():
+			var other_player = child.get_other_player()
+			child.set_is_able_to_merge(false)
+			
+			var cur_scale = child.get_player_scale()
+			var other_scale = other_player.get_player_scale()
+			print(cur_scale)
+			print(other_scale)
+			print()
+			child.set_player_scale(cur_scale + other_scale)
+			
+			other_player.set_is_dead()
+			player_children.erase(other_player)
+			
+func game_over():
+	print('well, rip')
+	is_over = false
+	set_physics_process(false)
