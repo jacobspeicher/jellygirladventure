0b228e5fc56fa518e5a2694618cf87539aaebae5
jacobspeicher <speicherjacob@gmail.com>
2018-08-09 00:44:16 -0400

    fixed and issue with collisions not registering when you land right on top, starting to think about a wall jump, trying to figure out a bounce-back when get hurt

--- char.gd
+++ char.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (int) var run_speed = 225
 export (int) var jump_speed = -415
-export (int) var bounce_back_force = 300
+export (int) var bounce_back_force = 20
 export (int) var gravity = 1200
 export (float) var size = 1.0
 export (bool) var able_to_merge = false
@@ -10,35 +10,93 @@ export (bool) var hit = false
 export (bool) var dead = false
 
 var velocity
+
 var jumping
+var wall_jumping
+var wall_stuck = false
+
+var on_floor = false
+var on_wall = false
+
 var initial_char_pos
+
 var hit_object
+var timer
+var knockback
+var direction = 'none'
 
 func _ready():
 	velocity = Vector2()
 	jumping = false
 	initial_char_pos = get_global_position()
+	knockback = false
+	
 	set_physics_process(true)
 		
 func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
+	if not get_is_knockback():
+		get_input()
+
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+	if is_on_wall() and not is_on_floor():
+		wall_stuck = true
+		jumping = false
+	else:
+		wall_stuck = false
+	
+	if not wall_stuck:
+		velocity.y += gravity * delta
+		
 	if jumping and is_on_floor():
 		jumping = false
+
+	if wall_jumping:
+		if is_on_wall():
+			wall_stuck = true
+			wall_jumping = false
+		if is_on_floor():
+			wall_jumping = false
 	
-	if get_slide_count() > 0:
-		var collision = get_slide_collision(get_slide_count() - 1)
-		hit_object = collision.collider
-		if hit_object.get_collision_layer() == 1:
-			able_to_merge = true
-		if hit_object.get_collision_layer() == 32:
-			set_is_hit(true)
-			velocity += collision.get_normal() * bounce_back_force
-			
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	var collision = []
+	for i in range(get_slide_count()):
+		collision.append(get_slide_collision(get_slide_count() - 1))
+	if collision:
+		#print(get_slide_count())
+		for col in collision:
+			hit_object = col.collider
+			var hit_layer = hit_object.get_collision_layer()
+			#print(hit_layer)
+
+			if hit_layer == 1:
+				set_is_able_to_merge(true)
+
+			if hit_layer == 2:
+				on_floor = true
+
+			if hit_layer == 4:
+				on_wall = true
+
+			if hit_layer == 8:
+				pass
+
+			if hit_layer == 16:
+				pass
+
+			if hit_layer == 32:
+				print('done been hit')
+				set_is_hit(true)
+				velocity = velocity.bounce(col.get_normal()) * bounce_back_force
+				print(get_collision_mask_bit(6))
+				set_collision_mask_bit(6, false)
+
+			if hit_layer == 64:
+				pass
 	
 func get_input():
-	velocity.x = 0
+	if velocity.x > 0 or velocity.x < 0:
+		velocity.x *= 0.5
+		
 	var right = Input.is_action_pressed('right')
 	var left = Input.is_action_pressed('left')
 	var jump = Input.is_action_just_pressed('jump')
@@ -47,11 +105,25 @@ func get_input():
 		jumping = true
 		velocity.y = jump_speed
 
+	if jump and wall_stuck and not is_on_floor():
+		wall_jumping = true
+		wall_stuck = false
+		velocity.y = jump_speed
+		if direction == 'left':
+			velocity.x = -jump_speed * 2
+		if direction == 'right':
+			velocity.x = jump_speed * 2
+
 	if not is_on_wall():
 		if right:
-			velocity.x += run_speed
-		if left:
-			velocity.x -= run_speed
+			direction = 'right'
+			velocity.x = run_speed
+		elif left:
+			direction = 'left'
+			velocity.x = -run_speed
+		else:
+			direction = 'none'
+	
 
 func get_player_scale():
 	return size
@@ -61,6 +133,9 @@ func get_is_hit():
 
 func get_is_able_to_merge():
 	return able_to_merge
+	
+func get_is_knockback():
+	return knockback
 	
 func get_other_player():
 	if able_to_merge:
@@ -75,6 +150,9 @@ func set_is_hit(new_hit):
 	
 func set_is_able_to_merge(new_able_to_merge):
 	able_to_merge = new_able_to_merge
+	
+func set_is_knockback(new_knockback):
+	knockback = new_knockback
 	
 func set_is_dead():
 	remove_child(get_node("CollisionShape2D"))
--- platformer_rules.gd
+++ platformer_rules.gd
@@ -2,6 +2,7 @@ extends Node
 
 var player_children = []
 var is_over = false
+var timer
 
 func _ready():
 	var player = load('res://player.tscn')
@@ -14,6 +15,11 @@ func _ready():
 	player_instance.set_global_position(Vector2(start_pos.x + 30, start_pos.y))
 	player_children.append(player_instance)
 	add_child(player_instance)
+	
+	timer = Timer.new()
+	timer.wait_time = 1
+	timer.one_shot = true
+	
 	set_physics_process(true)
 	
 func _physics_process(delta):
@@ -31,23 +37,19 @@ func _physics_process(delta):
 				else:
 					new_scale = child.get_player_scale() - 0.25
 				
-				child.set_is_hit(false)
-				child.set_player_scale(new_scale)
-				
 				var new_player = load('res://player.tscn')
 				var new_player_instance = new_player.instance()
 				var new_player_name = 'player' + str(len(player_children))
-				print(child.get_global_position())
-				var timer = Timer.new()
-				timer.one_shot = true
-				timer.wait_time = 1
-				timer.start()
-				
+
 				var new_player_pos = Vector2(child.get_global_position().x + 50, child.get_global_position().y)
 				print(new_player_pos)
 				new_player_instance.set_name(new_player_name)
 				new_player_instance.set_global_position(new_player_pos)
 				new_player_instance.set_player_scale(cur_scale - new_scale)
+				new_player_instance.set_collision_mask_bit(32, false)
+
+				child.set_is_hit(false)
+				child.set_player_scale(new_scale)
 				
 				player_children.append(new_player_instance)
 				add_child(new_player_instance)
@@ -58,16 +60,17 @@ func _physics_process(delta):
 		if child.get_is_able_to_merge():
 			var other_player = child.get_other_player()
 			child.set_is_able_to_merge(false)
-			
-			var cur_scale = child.get_player_scale()
-			var other_scale = other_player.get_player_scale()
-			print(cur_scale)
-			print(other_scale)
-			print()
-			child.set_player_scale(cur_scale + other_scale)
-			
-			other_player.set_is_dead()
-			player_children.erase(other_player)
+			print(other_player)
+			print(weakref(other_player).get_ref())
+			if not weakref(other_player).get_ref() == null:
+				var cur_scale = child.get_player_scale()
+				var other_scale = other_player.get_player_scale()
+				print(cur_scale)
+				print(other_scale)
+				child.set_player_scale(cur_scale + other_scale)
+				
+				other_player.set_is_dead()
+				player_children.erase(other_player)
 			
 func game_over():
 	print('well, rip')
